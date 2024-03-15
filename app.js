c1693a8d407addbf4c4af64dd7377a9fada41770
tauseef-creator <tauseefrazaq71@gmail.com>
2024-03-15 12:24:40 +0500

    add buttons to navigate

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(`
+        <h1>Hello World!</h1>
+        <a href="/about"><button>Go to About Page</button></a>
+    `);
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.send(`
+        <h1>About page</h1>
+        <a href="/"><button>Go to Home Page</button></a>
+    `);
 });
 
 app.listen(port, () => {
